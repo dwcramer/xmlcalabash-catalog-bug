4adfbe85e1c2976459ee0d3b38623f6135618b81
David Cramer <david@thingbag.net>
2017-09-15 12:18:01 -0500

    Workaround using apache resolver

--- build.sh
+++ build.sh
@@ -7,3 +7,14 @@ echo "==========================================="
 echo "book-fails.xml fails:"
 
 java -Xmx1024m -cp "/Applications/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar:./shared/catalog" com.xmlcalabash.drivers.Main -i source=doc/book-fails.xml xpl/pipeline.xpl
+
+echo " "
+echo "==========================================="
+echo "Workaround book-fails.xml succeeds using Apache resolver:"
+
+java -Xmx1024m  -cp "/Applications/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar:/Applications/Oxygen XML Editor/lib/resolver.jar:./shared/catalog" \
+                com.xmlcalabash.drivers.Main \
+                -i source=doc/book-fails.xml \
+                -U org.apache.xml.resolver.tools.CatalogResolver \
+                -E org.apache.xml.resolver.tools.CatalogResolver \
+                xpl/pipeline.xpl
